6788df924909b23a3222c14a1ae208ed137ac20d
solingoran99 <solin.goran.yusuf.rasid@iths.se>
2024-09-20 00:11:01 +0200

    corrected lab

--- NumbersLab1(corrected)/Program.cs
+++ NumbersLab1(corrected)/Program.cs
@@ -10,29 +10,23 @@ namespace NumbersLab1_corrected_
 			long totalSum = 0;
 
 			Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Patterns that start and end with the same digit.");
+			Console.WriteLine("Patterns that start and end with the same digit.");
 			Console.ResetColor();
 
-			for (char digit = '1'; digit <= '9'; digit++)
+			
+			for (int startIndex = 0; startIndex < myNumbers.Length; startIndex++)
 			{
-				int startIndex = 0;
-				
-				while (startIndex < myNumbers.Length)
+				char startDigit = myNumbers[startIndex];
+
+				if (char.IsDigit(startDigit))
 				{
-					int startMatch = myNumbers.IndexOf(digit, startIndex);
+					int endIndex = myNumbers.IndexOf(startDigit, startIndex + 1);
 
-					if (startMatch == -1)
+					while (endIndex != -1)
 					{
-						break;
-					}
-
-					int endMatch = myNumbers.IndexOf(digit, startMatch + 1);
-
-					while (endMatch != -1)
-					{
-						
-						string matchedNumbers = myNumbers.Substring(startMatch, endMatch - startMatch + 1);
+						string matchedNumbers = myNumbers.Substring(startIndex, endIndex - startIndex + 1);
 						string middlePart = matchedNumbers.Substring(1, matchedNumbers.Length - 2);
+
 						bool isValid = true;
 
 						foreach (char c in middlePart)
@@ -44,38 +38,35 @@ namespace NumbersLab1_corrected_
 							}
 						}
 
-						if (isValid)
+						if (isValid && !middlePart.Contains(startDigit))
 						{
-                            Console.Write(myNumbers.Substring(0, startMatch));
+							Console.Write(myNumbers.Substring(0, startIndex));
 							Console.ForegroundColor = ConsoleColor.Magenta;
-                            Console.Write(matchedNumbers);
+							Console.Write(matchedNumbers);
 							Console.ResetColor();
-                            Console.WriteLine(myNumbers.Substring(endMatch + 1));
+							Console.WriteLine(myNumbers.Substring(endIndex + 1));
 
-							long matchedNums;
-							if (long.TryParse(matchedNumbers, out matchedNums))
+							if (long.TryParse(matchedNumbers, out long matchedNums))
 							{
 								totalSum += matchedNums;
 							}
 
-							startIndex = endMatch + 1;
-							break;
-                        }
+							endIndex = myNumbers.IndexOf(startDigit, endIndex + 1);
+						}
 						else
 						{
-							endMatch = myNumbers.IndexOf(digit, endMatch + 1);
+							endIndex = myNumbers.IndexOf(startDigit, endIndex + 1);
 						}
 					}
-
 				}
 			}
 
-            Console.ForegroundColor= ConsoleColor.Red;
-            Console.WriteLine($"\nTotal = {totalSum}");
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine($"\nTotal = {totalSum}");
 			Console.ResetColor();
 
 			Console.ReadKey();
-
-        }
+		}
 	}
 }
+
